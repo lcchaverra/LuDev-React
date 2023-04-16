10bf1e5fa06736ea77acf4fea5031b88d86cbca5
Luis Carlos Chaverra Cordoba <lucas.cordoba1510@gmail.com>
2023-04-16 00:49:52 -0500

    correcion: cambios en la secion de experiencias

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -14,6 +14,7 @@ const Experience = () => {
                             <li><strong>Programador Freelance (2022)</strong> <br/> Proyecto - Aplicatio Web Para El Centro de Emprendimiento e Innovacion "CEI" <br/> <em>Participe en la elaboracion del aplicativo web para el CEI de la universidad tecnologica del chocó, donde mi rol fue ser FullStack</em></li>
                             <li><strong>Facturador de Farmacia (Sep 2019 - Nov 2019)</strong><br/> Drogueria Freymar <br/> <em>Procesamiento de documentación e introducción de datos en bases de datos.Revisión de Idoneidad de documentos y control de archivo.</em></li>
                             <li><strong>Uno de los Ganadores de la Competencia de Desarrollo - Hack The World (2022)</strong> - RioSoftware</li>
+                            <li><strong>1er puesto en Competencia de desarrollo  - FlyChallenge (2023)</strong> - HelppeopleCloud</li>
                         </ul>
                 </div>
             </div>
