997d1fb0dc56e55507a23516d2e710dd797c48c7
Luis Carlos Chaverra Cordoba <lucas.cordoba1510@gmail.com>
2023-08-23 00:19:45 -0500

    update education

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -10,9 +10,10 @@ const Education = () => {
                     <img className="empty-2" src={EducationImg} alt="Mi foto" />
                         <ul>
                             <li><strong>Ingeniero de Sistemas (2023)</strong> - Fundacion Universitaria Claretiana "Uniclaretiana"</li>
+                            <li><strong>Diplomado en Docencia y Didáctica Universitaria</strong> - Politécnico Superior de Colombia</li>
                             <li><strong>Curso de Desarrollo Web Front end HTML y CSS (2021)</strong> - Crehana</li>
                             <li><strong>Bootcamp de Desarrollo de software (2023)</strong> - Rio Software / Escuela de Robótica del Chocó</li>
-                            <li>Nivel <strong>B1</strong> en Inglés (Certificado)</li>
+                            <li>Nivel <strong>B1</strong> en Inglés</li>
                         </ul>
                 </div>
             </div>
